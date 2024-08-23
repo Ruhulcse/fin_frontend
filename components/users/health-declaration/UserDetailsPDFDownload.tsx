26fed41004098c930476dc21197b0715ecafba39
Tareq <tareqnmd@gmail.com>
2024-08-24 01:26:23 +0600

    update

--- components/users/health-declaration/UserDetailsPDFDownload.tsx
+++ components/users/health-declaration/UserDetailsPDFDownload.tsx
@@ -1,29 +1,32 @@
-"use client";
-import { cn } from "@/lib/utils";
-import { PDFDownloadLink } from "@react-pdf/renderer";
-import UserDetailsPDF from "./UserDetailsPDF";
-import { useTranslations } from "next-intl";
+'use client';
+import { cn } from '@/lib/utils';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import { useTranslations } from 'next-intl';
+import UserDetailsPDF from './UserDetailsPDF';
 
 const UserDetailsPDFDownload = ({
-  userDetails,
-  children,
+	userDetails,
+	children,
 }: {
-  userDetails: any;
-  children?: React.ReactNode;
+	userDetails: any;
+	children?: React.ReactNode;
 }) => {
-  const t = useTranslations("agreement");
-  const userDetl = t("userDetails");
-  return (
-    <PDFDownloadLink
-      document={
-        <UserDetailsPDF userDetails={userDetails} userDetl={userDetl} />
-      }
-      fileName="user-details.pdf"
-      className={cn(children ? "" : "text-secondary underline w-max h-max")}
-    >
-      {children ?? userDetl}
-    </PDFDownloadLink>
-  );
+	const t = useTranslations('agreement');
+	const userDetl = t('userDetails');
+	return (
+		<PDFDownloadLink
+			document={
+				<UserDetailsPDF
+					userDetails={userDetails}
+					userDetl={userDetl}
+				/>
+			}
+			fileName="user-details.pdf"
+			className={cn(children ? '' : 'text-secondary underline w-max h-max')}
+		>
+			{children ?? t('userDetails')}
+		</PDFDownloadLink>
+	);
 };
 
 export default UserDetailsPDFDownload;
