f250a7a7d95d48c6eb7fbf759f7bd26799d88a57
Tareq <tareqnmd@gmail.com>
2024-08-25 00:43:14 +0600

    updated

--- store/theme/slice.ts
+++ store/theme/slice.ts
@@ -2,7 +2,7 @@ import { RootState } from '@/store';
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-	locale: 'en',
+	locale: 'he',
 };
 
 const themeSlice = createSlice({
