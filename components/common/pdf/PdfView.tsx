fc4de699347c9ace881838bc77e71f932f3e5c95
Tareq <tareqnmd@gmail.com>
2024-08-28 18:29:45 +0600

    update

--- components/common/pdf/PdfView.tsx
+++ components/common/pdf/PdfView.tsx
@@ -1,7 +1,20 @@
+'use client';
+import { useEffect, useState } from 'react';
+
 const PdfView = ({ pdf }: { pdf: string }) => {
-	return pdf ? (
+	const [url, setUrl] = useState<string>('');
+
+	useEffect(() => {
+		if (pdf) {
+			const pdfUrl =
+				pdf + `#toolbar=0${window.innerWidth < 991 ? '&zoom=28' : ''}`;
+			setUrl(pdfUrl);
+		}
+	}, [pdf]);
+
+	return url ? (
 		<iframe
-			src={pdf + '#toolbar=0'}
+			src={url}
 			className="rounded-lg h-dvh w-full"
 		/>
 	) : null;
