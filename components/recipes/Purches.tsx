efd24b90bd0daa1dde59b6ce401f9378fa64c40e
abircu <mdabircse4873@gmail.com>
2024-11-07 22:29:47 +0600

    purchases and success pages i18 implemented

--- components/recipes/Purches.tsx
+++ components/recipes/Purches.tsx
@@ -1,7 +1,9 @@
 'use client';
+import { useTranslations } from 'next-intl';
 import React from 'react'
 
 const Purches = () => {
+  const t=useTranslations('recepie')
   const handlePurchase=()=>{
     console.log("click in purchase button")
   }
@@ -9,7 +11,7 @@ const Purches = () => {
     <div className=''> 
       <div className='flex justify-center items-center min-h-screen'>
        <div className='bg-gradient-to-r from-blue-500 to-green-500 rounded-lg  flex justify-center items-center'>
-          <button onClick={handlePurchase} className='text-white px-10 py-5 text-2xl font-bold text-center'>Purchase</button>
+          <button onClick={handlePurchase} className='text-white px-10 py-5 text-2xl font-bold text-center'>{t('buttonText')}</button>
        </div>
       </div>
     </div>
--- components/recipes/Success.tsx
+++ components/recipes/Success.tsx
@@ -1,17 +1,19 @@
+import { useTranslations } from 'next-intl';
 import React from 'react'
 import { FaCheck } from 'react-icons/fa';
 const Success = () => {
+  const t=useTranslations('recepie')
   return (
     <div className='min-h-screen flex flex-col justify-center items-center'>
         <div className=' flex  justify-center items-center gap-3'>
             <FaCheck className='  bg-green-700 rounded-full text-4xl ' />
-            <h1 className='text-green-700 font-semibold text-4xl '>Payment succesfull</h1>
+            <h1 className='text-green-700 font-semibold text-4xl '>{t('succesTitle')}</h1>
         </div>
         <div className='flex justify-center items-center pt-3'>
-            <p className='text-black text-2xl font-semibold'>Thank you! Your payment has been received.</p>
+            <p className='text-black text-2xl font-semibold'>{t('successSubTitle')}</p>
         </div>
         <div className=' flex justify-center items-center pt-10'>
-            <input type="email" name="" id="" className='mt-10  w-96 rounded-tl-lg rounded-bl-lg px-4 py-2 bg-gray-300 text-black placeholder:text-gray-700' placeholder='Plese enter your email'/><button className='bg-gradient-to-tr mt-10 px-4 py-2 from-sky-700 to-sky-500 rounded-tr-lg rounded-br-lg'>Submit</button>
+            <input type="email" name="" id="" className='mt-10  w-96 rounded-tl-lg rounded-bl-lg px-4 py-2 bg-gray-300 text-black placeholder:text-gray-700' placeholder={`${t('placeholder')}`} /><button className='bg-gradient-to-tr mt-10 px-4 py-2 from-sky-700 to-sky-500 rounded-tr-lg rounded-br-lg'>{t('successButton')}</button>
         </div>
     </div>
   )
